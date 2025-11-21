8dc92eda377aad4b4a529cb31dc38923a8dd2a93
marineags <agsmarine23@gmail.com>
2025-11-21 01:00:30 +0100

    update todo

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import TodoItem from "./TodoItem";
+import { Construction } from "lucide-react";
 // ⚠️ne pas l'oublier
 type Priority = "Urgente" | "Moyenne" | "Basse";
 
@@ -16,6 +18,7 @@ function App() {
   const savedTodos = localStorage.getItem("todos");
   const initialTodos = savedTodos ? JSON.parse(savedTodos) : [];
   const [todos, setTodos] = useState<Todo[]>(initialTodos);
+  const [filter, setFilter] = useState<Priority | "Tous">("Tous");
   // cle et valeurs localstorage
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -33,18 +36,32 @@ function App() {
 
     const newTodos = [newTodo, ...todos]; // on ajoute en premier
     setTodos(newTodos);
-
     // reset des champs
     setInput("");
     setPriority("Moyenne");
-
     console.log(newTodos);
   }
 
+  let filteredTodos: Todo[] = [];
+  if (filter === "Tous") {
+    filteredTodos = todos;
+  } else {
+    filteredTodos = todos.filter((todo) => todo.priority === filter);
+  }
+
+  const urgentCount = todos.filter((t) => t.priority === "Urgente").length;
+  const mediumCount = todos.filter((t) => t.priority === "Moyenne").length;
+  const lowCount = todos.filter((t) => t.priority === "Basse").length;
+  const totalCount = todos.length;
+
+  function deleteTodo(id: number) {
+    // 💡 syntaxe corrigée et opérateur !==
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  }
   return (
     //pour que le formulaire soit centrer
     <div className="flex justify-center">
-      {/* pour la taille : rounded pour les  arrondits :p padding */}
       <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2x1">
         <div className="flex gap-4">
           {/* vient de daisy ui : w-full pour que sa prenne la largeur */}
@@ -69,6 +86,61 @@ function App() {
           <button onClick={addTodo} className="btn btn-primary">
             ajouter
           </button>
+        </div>
+        <div className="space-y-2 flex-1 h-fit">
+          <div className="flex flex-wrap gap-4">
+            <button
+              className={`btn btn-soft ${
+                filter === "Tous" ? "btn-primary" : ""
+              }`}
+              onClick={() => setFilter("Tous")}
+            >
+              Tous ({totalCount})
+            </button>
+            <button
+              className={`btn btn-soft ${
+                filter === "Urgente" ? "btn-primary" : ""
+              }`}
+              onClick={() => setFilter("Urgente")}
+            >
+              Urgent ({urgentCount})
+            </button>
+            <button
+              className={`btn btn-soft ${
+                filter === "Moyenne" ? "btn-primary" : ""
+              }`}
+              onClick={() => setFilter("Moyenne")}
+            >
+              Moyenne ({mediumCount})
+            </button>
+            <button
+              className={`btn btn-soft ${
+                filter === "Basse" ? "btn-primary" : ""
+              }`}
+              onClick={() => setFilter("Basse")}
+            >
+              Basse ({lowCount})
+            </button>
+          </div>
+          {filteredTodos.length > 0 ? (
+            <ul className="divide-y divide-primary/20">
+              {filteredTodos.map((todo) => (
+                <li key={todo.id}>
+                  <TodoItem todo={todo} onDelete={() => deleteTodo(todo.id)} />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="flex justify-center items-center flex-col p-5">
+              <div>
+                <Construction
+                  strokeWidth={1}
+                  className="w-40 h-40 text-primary"
+                />
+              </div>
+              <p className="text-sm">aucune tache pour le moment</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
---
+++ todo/src/TodoItem.tsx
@@ -0,0 +1,48 @@
+import { Trash } from "lucide-react";
+
+type Priority = "Urgente" | "Moyenne" | "Basse";
+
+type Todo = {
+  id: number;
+  text: string;
+  priority: Priority;
+};
+type Props = {
+  todo: Todo;
+  onDelete: () => void;
+};
+
+const TodoItem = ({ todo, onDelete }: Props) => {
+  return (
+    <li className="p-3">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            className="checkbox checkbox-primary checkbox-sm"
+          />
+          <span className="text-md font bold">
+            <span>{todo.text}</span>
+          </span>
+          <span
+            className={`badge badge-sm badge-soft
+            ${
+              todo.priority === "Urgente"
+                ? "badge-error"
+                : todo.priority === "Moyenne"
+                ? "badge-warning"
+                : "badge-success"
+            }`}
+          >
+            {todo.priority}
+          </span>
+        </div>
+        <button onClick={onDelete} className="btn btn-sm btn-error btn-soft">
+          <Trash className="w-4 h-4" />
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default TodoItem;
